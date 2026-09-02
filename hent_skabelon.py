514aa370cf0e5a2d2806bb216a0dc65bb2a8b185
REKSTAB-Analyse <ascp@adm.ku.dk>
2026-09-02 13:41:24 +0200

    wd er forsøgt fikset

--- hent_skabelon.py
+++ hent_skabelon.py
@@ -1,8 +1,14 @@
 import os
 import urllib.request
 
+qmd_sti = os.environ.get("QUARTO_DOCUMENT_PATH")
+if qmd_sti:
+    skabelon_mappe = os.path.dirname(os.path.abspath(qmd_sti))
+else:
+    skabelon_mappe = os.getcwd()
+
 URL = f"{base_url}/ku_skabelon.tex"
-DEST = "ku_skabelon.tex"
+DEST = os.path.join(skabelon_mappe, "ku_skabelon.tex")
 
 if os.path.exists(DEST):
     print("Bruger lokal kopi af ku_skabelon.tex")
